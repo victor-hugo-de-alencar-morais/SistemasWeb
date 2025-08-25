292b5512971f092fdc411135555eacf1afab4536
victor-hugo-de-alencar-morais <vham17.com@gmail.com.br>
2025-08-25 20:45:20 -0300

    teste

---
+++ Sistemas-Web-I-main/Projeto Escola/escola/src/main/java/br/com/projetoescola/escola/controller/EscolaController.java
@@ -0,0 +1,17 @@
+package br.com.projetoescola.escola.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/escola")
+public class EscolaController {
+
+    @GetMapping
+    public String index(Model model) {
+        return "index";
+    }
+    
+}
