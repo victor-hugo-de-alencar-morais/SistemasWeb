6a7a1a3560f07752b8618622f6a5d9b0ea195d1f
victor-hugo-de-alencar-morais <vham17.com.br@gmail.com>
2025-09-11 20:19:06 -0300

    novo formulario

--- Sistemas-Web-I-main/Projeto Escola/escola/src/main/java/br/com/projetoescola/escola/controller/CursoController.java
+++ Sistemas-Web-I-main/Projeto Escola/escola/src/main/java/br/com/projetoescola/escola/controller/CursoController.java
@@ -1,17 +1,44 @@
 package br.com.projetoescola.escola.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.projetoescola.escola.Service.CursoService;
+import br.com.projetoescola.escola.entity.Curso;
+
 @Controller
-@RequestMapping("/curso")
+@RequestMapping("/cursos")
 public class CursoController {
 
-    @GetMapping
-    public String index(Model model) {
-        return "index";
+    //Injeção de dependência da service de cursos
+    @Autowired
+    private CursoService cursoService;
+ 
+    //Método para salvar um curso
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute Curso curso) {
+        cursoService.save(curso);
+        return "redirect:/cursos/listar";
+    }
+    //Método para listar todos os cursos
+    @GetMapping("/listar")
+    public String listar(Model model){
+        List<Curso> curso = cursoService.findAll();
+        model.addAttribute("cursos", curso);
+        return "curso/listarCurso";
+    }
+    //Método para abrir o formulário de curso
+    @GetMapping("/criar")
+    public String criarForm(Model model) {
+        model.addAttribute("aluno", new Curso());
+        return "curso/formularioCurso";
     }
     
 }
